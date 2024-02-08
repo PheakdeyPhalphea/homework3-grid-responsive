e71ff5c364783672f3a339addecda2bcc7b235b9
kdey <phalpheapheakdey@gmail.com>
2024-02-08 21:53:58 +0700

    fix js bug

--- scripte.js
+++ scripte.js
@@ -1,7 +1,21 @@
 function myFunction(img) {
-  // Set the full-screen image source
   document.getElementById("expandedImg").src = img.src;
 
   // Show the full-screen section
-  document.querySelector(".gallary-full-screen .container").style.display = "block";
+  var fullScreenContainer = document.querySelector(".gallary-full-screen");
+  fullScreenContainer.style.display = "block";
+
+  // Function to close the full-screen section
+  function closeFullScreen() {
+    fullScreenContainer.style.display = "none";
+    // Remove the event listener to prevent it from interfering with subsequent clicks
+    fullScreenContainer.removeEventListener("click", closeFullScreen);
+  }
+
+  var closeButton = document.querySelector(".closebtn");
+  closeButton.addEventListener("click", function (event) {
+    // Prevent the click event from bubbling up to the parent container
+    event.stopPropagation();
+    fullScreenContainer.style.display = "none";
+  });
 }
